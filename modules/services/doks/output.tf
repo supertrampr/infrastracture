c59cf1382f95b0584f2ad96d23d6c5a6f90a049e
SupertrampR <piccolo.raffaele@outlook.it>
2022-12-06 15:20:03 +0100

    Add output for doks terraform module.

---
+++ modules/services/doks/output.tf
@@ -0,0 +1,12 @@
+output "host" {
+  value = digitalocean_kubernetes_cluster.example.kube_config.0.host
+}
+
+output "token" {
+  value = digitalocean_kubernetes_cluster.example.kube_config.0.token
+}
+
+output "cluster_ca_certificate" {
+  value     = digitalocean_kubernetes_cluster.example.kube_config.0.cluster_ca_certificate
+  sensitive = true
+}
