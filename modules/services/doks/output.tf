0359e056677b27c4b2aa856f8e5ab05754df0ba5
SupertrampR <piccolo.raffaele@outlook.it>
2022-12-07 22:43:30 +0100

    Add output message to show how to connect to DOKS cluster.

--- modules/services/doks/output.tf
+++ modules/services/doks/output.tf
@@ -10,3 +10,7 @@ output "cluster_ca_certificate" {
   value     = digitalocean_kubernetes_cluster.example.kube_config.0.cluster_ca_certificate
   sensitive = true
 }
+
+output "id" {
+  value = digitalocean_kubernetes_cluster.example.id
+}
