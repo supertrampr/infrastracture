c0f0104c7d076d00c45f0552fb909db2305dc1cb
SupertrampR <piccolo.raffaele@outlook.it>
2022-12-07 15:02:17 +0100

    Add ovh terraform module and references.

---
+++ hello-world/live/variable-ovh.tf
@@ -0,0 +1,14 @@
+variable "ovh_application_key" {
+  description = "OVH Application Key to communicate with API."
+  type        = string
+}
+
+variable "ovh_application_secret" {
+  description = "OVH Application Secret to communicate with API."
+  type        = string
+}
+
+variable "ovh_consumer_key" {
+  description = "OVH Consumer Key to communicate with API."
+  type        = string
+}
